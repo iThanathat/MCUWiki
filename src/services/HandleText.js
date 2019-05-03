3b43b1bd9a10d0977823c08b3d5e13ce9c429a24
Panitan <panitan.wt@gmail.com>
2019-05-03 16:38:00 +0700

    add folder mockdb + file characterDb.js AND update file HandleText

--- src/services/HandleText.js
+++ src/services/HandleText.js
@@ -1,3 +1,4 @@
+var characterInfo = require('../mockdb/characterDb').CharacterInfomations;
 var replyServices = require('./ReplyServices');
 
 module.exports = function (client, message, replyToken){
@@ -11,5 +12,28 @@ module.exports = function (client, message, replyToken){
 
 function setReplyTextFomat(textMsgs) {
     textMsgs = Array.isArray(textMsgs) ? textMsgs : [textMsgs];
-    return textMsgs.map((text) => ({type: 'text', text}));
+    return textMsgs.map((text) => ({type: 'text', text: getCharaterInfo(text)}));
+}
+
+function getCharaterInfo(textMsgs) {
+  let character = characterInfo.find(characterData => {
+    if (compareStringIgnoreCase(characterData.Name, textMsgs)) {
+      return characterData;
+    }
+  });
+  if (character === undefined) {
+    character = "Not found this character please try another character";
+  return character;
+  } else {
+  return character.Name + '\n' + character.Type + '\n' + character.Info;
+  }
+}
+
+
+function compareStringIgnoreCase(str1, str2) {
+  let equal = false;
+  if (str1.toLowerCase() == str2.toLowerCase()) {
+    equal = true;
+  }
+  return equal;
 }
